154ec9b9d9beb6317a4361748a49203fcbaef2de
Nguyễn Huỳnh Phú Vinh <tomisakaeanime@gmail.com>
2024-02-26 11:02:57 +0700

    Thêm web tiến trình

--- IsekaiQuest/scripts/ChuyenTiep.js
+++ IsekaiQuest/scripts/ChuyenTiep.js
@@ -9,26 +9,26 @@ function ChuyenTiep() {
         document.getElementById("chuyen_tiep1").classList.add("d-none");
         document.getElementById("chuyen_tiep2").classList.remove("d-none");
     }
-    else if (dem == 1) {
-        document.getElementById("chuyen_tiep2").classList.add("d-none");
-        document.getElementById("chuyen_tiep3").classList.remove("d-none");
-    }
-    else if (dem == 2) {
-        document.getElementById("chuyen_tiep3").classList.add("d-none");
-        document.getElementById("chuyen_tiep4").classList.remove("d-none");
-    }
-    else if (dem == 3) {
-        document.getElementById("chuyen_tiep4").classList.add("d-none");
-        document.getElementById("chuyen_tiep5").classList.remove("d-none");
-    }
-    else if (dem == 4) {
-        document.getElementById("chuyen_tiep5").classList.add("d-none");
-        document.getElementById("chuyen_tiep6").classList.remove("d-none");
-    }
-    else if (dem == 5) {
-        document.getElementById("chuyen_tiep6").classList.add("d-none");
-        document.getElementById("chuyen_tiep7").classList.remove("d-none");
-    }
+    // else if (dem == 1) {
+    //     document.getElementById("chuyen_tiep2").classList.add("d-none");
+    //     document.getElementById("chuyen_tiep3").classList.remove("d-none");
+    // }
+    // else if (dem == 2) {
+    //     document.getElementById("chuyen_tiep3").classList.add("d-none");
+    //     document.getElementById("chuyen_tiep4").classList.remove("d-none");
+    // }
+    // else if (dem == 3) {
+    //     document.getElementById("chuyen_tiep4").classList.add("d-none");
+    //     document.getElementById("chuyen_tiep5").classList.remove("d-none");
+    // }
+    // else if (dem == 4) {
+    //     document.getElementById("chuyen_tiep5").classList.add("d-none");
+    //     document.getElementById("chuyen_tiep6").classList.remove("d-none");
+    // }
+    // else if (dem == 5) {
+    //     document.getElementById("chuyen_tiep6").classList.add("d-none");
+    //     document.getElementById("chuyen_tiep7").classList.remove("d-none");
+    // }
     else {
         let ten_nhan_vat = document.getElementById("nhap_ten").value;
 
