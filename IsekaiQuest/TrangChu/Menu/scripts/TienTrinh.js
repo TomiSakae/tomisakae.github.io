79817b6a4812380a90fccbe515097f6f6fceefdf
Nguyễn Huỳnh Phú Vinh <tomisakaeanime@gmail.com>
2024-02-27 11:28:05 +0700

    Xóa 2 menu và thêm vue.js

--- IsekaiQuest/TrangChu/Menu/scripts/TienTrinh.js
+++
@@ -1,7 +0,0 @@
-let tien = localStorage.getItem("tien");
-
-if (tien == null) {
-    tien = 0;
-}
-
-document.getElementById("tien").innerHTML = "Tiền: " + tien + " VNĐ"
--- IsekaiQuest/scripts/ChuyenTiep.js
+++ IsekaiQuest/scripts/ChuyenTiep.js
@@ -8,28 +8,7 @@ function ChuyenTiep() {
     if (dem == 0) {
         document.getElementById("chuyen_tiep1").classList.add("d-none");
         document.getElementById("chuyen_tiep2").classList.remove("d-none");
-    }
-    // else if (dem == 1) {
-    //     document.getElementById("chuyen_tiep2").classList.add("d-none");
-    //     document.getElementById("chuyen_tiep3").classList.remove("d-none");
-    // }
-    // else if (dem == 2) {
-    //     document.getElementById("chuyen_tiep3").classList.add("d-none");
-    //     document.getElementById("chuyen_tiep4").classList.remove("d-none");
-    // }
-    // else if (dem == 3) {
-    //     document.getElementById("chuyen_tiep4").classList.add("d-none");
-    //     document.getElementById("chuyen_tiep5").classList.remove("d-none");
-    // }
-    // else if (dem == 4) {
-    //     document.getElementById("chuyen_tiep5").classList.add("d-none");
-    //     document.getElementById("chuyen_tiep6").classList.remove("d-none");
-    // }
-    // else if (dem == 5) {
-    //     document.getElementById("chuyen_tiep6").classList.add("d-none");
-    //     document.getElementById("chuyen_tiep7").classList.remove("d-none");
-    // }
-    else {
+    } else {
         let ten_nhan_vat = document.getElementById("nhap_ten").value;
 
         if (ten_nhan_vat != "") {
