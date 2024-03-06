b3f7347e7c0525a926608ac4485542523e221555
Nguyễn Huỳnh Phú Vinh <tomisakaeanime@gmail.com>
2024-03-06 19:50:44 +0700

    Xong 2 ví dụ lưới và bảng

---
+++ Nhom2TKW/TrangVD/Vue/Luoi.js
@@ -0,0 +1,94 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            vd1: true,
+            vd2: false,
+            vd3: false,
+            vd4: false,
+            vd5: false,
+            vd6: false,
+            vd7: false,
+            vd8: false,
+            vd_dau: false,
+            an_tiep: true,
+            vd_cuoi: false,
+            dem: 1
+        }
+    },
+    methods: {
+        ViDuKe() {
+            switch (this.dem) {
+                case 1:
+                    this.vd1 = false
+                    this.vd2 = true
+                    this.vd_dau = true
+                    break
+                case 2:
+                    this.vd2 = false
+                    this.vd3 = true
+                    break
+                case 3:
+                    this.vd3 = false
+                    this.vd4 = true
+                    break
+                case 4:
+                    this.vd4 = false
+                    this.vd5 = true
+                    break
+                case 5:
+                    this.vd5 = false
+                    this.vd6 = true
+                    break
+                case 6:
+                    this.vd6 = false
+                    this.vd7 = true
+                    break
+                case 7:
+                    this.vd7 = false
+                    this.vd8 = true
+                    this.vd_cuoi = true;
+                    this.an_tiep = false;
+                    break
+            }
+            this.dem++
+        },
+        ViDuTruoc() {
+            this.dem--
+            switch (this.dem) {
+                case 1:
+                    this.vd1 = true
+                    this.vd2 = false
+                    this.vd_dau = false
+                    break
+                case 2:
+                    this.vd2 = true
+                    this.vd3 = false
+                    break
+                case 3:
+                    this.vd3 = true
+                    this.vd4 = false
+                    break
+                case 4:
+                    this.vd4 = true
+                    this.vd5 = false
+                    break
+                case 5:
+                    this.vd5 = true
+                    this.vd6 = false
+                    break
+                case 6:
+                    this.vd6 = true
+                    this.vd7 = false
+                    break
+                case 7:
+                    this.vd7 = true
+                    this.vd8 = false
+                    this.vd_cuoi = false;
+                    this.an_tiep = true;
+                    break
+            }
+        }
+    }
+})
+
+app.mount('#app')
