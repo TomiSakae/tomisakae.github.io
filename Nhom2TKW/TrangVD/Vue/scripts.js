d8c58341833c5a673ecef91ffe472f50b5253711
Nguyễn Huỳnh Phú Vinh <tomisakaeanime@gmail.com>
2024-03-06 09:15:41 +0700

    Tạo ví dụ danh sách

---
+++ Nhom2TKW/TrangVD/Vue/scripts.js
@@ -0,0 +1,36 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            vd1: true,
+            vd2: false,
+            vd_dau: false,
+            dem: 1
+        }
+    },
+    methods: {
+        ViDuKe() {
+            switch (this.dem) {
+                case 1:
+                    this.vd1 = false
+                    this.vd2 = true
+                    this.vd_dau = true
+                    break
+
+            }
+            this.dem++
+        },
+        ViDuTruoc() {
+            this.dem--
+            switch (this.dem) {
+                case 1:
+                    this.vd1 = true
+                    this.vd2 = false
+                    this.vd_dau = false
+                    break
+
+            }
+        }
+    }
+})
+
+app.mount('#app')
