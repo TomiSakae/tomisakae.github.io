f83bc01b65c580b2ca1c83e435b5d1992619dd17
Nguyễn Huỳnh Phú Vinh <tomisakaeanime@gmail.com>
2024-03-13 07:07:21 +0700

    Tạm biệt Isekai Quest

--- IsekaiQuest/TrangChu/scripts/DLIsekaiQuest.js
+++
@@ -1,5 +0,0 @@
-let vi_tri = localStorage.getItem("noi_hien_tai");
-
-if (vi_tri == 1) {
-    window.location.replace("Menu/IsekaiQuest/Steamforest.html");
-}
--- IsekaiQuest/scripts/KiemTraDL.js
+++
@@ -1,7 +0,0 @@
-if (localStorage.getItem("ten_nv") != null) {
-    window.location.replace("TrangChu/TrangChu.html");
-}
-else {
-    window.location.replace("IsekaiQuest.html");
-    localStorage.setItem("csdl", 0);
-}
