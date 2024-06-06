1a4561dbde2b023453bc5f58eb6a93e8fe1ecd61
TomiSakae <tomisakaeap1@gmail.com>
2024-06-06 17:02:26 +0700

    thêm trang

---
+++ scripts.js
@@ -0,0 +1,15 @@
+$(function () {
+
+    let trang = Number(localStorage.getItem("trang"));
+    if (trang == null) {
+        localStorage.setItem("trang", 1);
+        trang = 1;
+    }
+
+    switch (trang) {
+        case 1:
+            window.location.href = "Trang1/index.html";
+            break;
+    }
+
+});
