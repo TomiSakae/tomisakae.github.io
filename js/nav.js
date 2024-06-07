585e929770cf9f2cf634dd51409d37c323492900
TomiSakae <tomisakaeap1@gmail.com>
2024-06-07 18:28:24 +0700

    cập nhật nav

--- js/nav.js
+++ js/nav.js
@@ -1,16 +1,23 @@
+
+$("#nav").html(`
+<nav class="container py-4 d-flex justify-content-between align-items-center">
+    <a href="#" class="h4 text-dark text-decoration-none">TomiSakae</a>
+    <div id="mobile-nav"></div>
+    <button class="nut-nav py-2 px-3"><span id="mobile-menu">Tài Khoản</span></button>
+</nav>
+`);
+
 function Mobile() {
     var width = $(window).width();
-    $(".mobile-br").empty(); // Xóa các phần tử đã thêm trước đó
 
     if (width <= 768) {
-        $(".mobile-br").append("<br>");
+        $(".mobile-br").html("<br>");
         $("#mobile-menu").text("Menu");
         $("#mobile-nav").empty();
     }
 
     if (width > 768) {
-        $("#mobile-nav").empty();
-        $("#mobile-nav").append(`
+        $("#mobile-nav").html(`
             <a href="#" class="link-nav px-3 py-2 mx-1">Nhân Vật</a>
             <a href="#" class="link-nav px-3 py-2 mx-1">Nhiệm Vụ</a>
             <a href="#" class="link-nav px-3 py-2 mx-1">Phiêu Lưu</a>
@@ -24,8 +31,6 @@ function Mobile() {
 
 $(function () {
     Mobile();
-
-
 });
 
 $(window).resize(function () {
