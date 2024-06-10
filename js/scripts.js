03d3360dda8e281d55e80aa57fc99395b80d5660
TomiSakae <tomisakaeap1@gmail.com>
2024-06-10 23:17:00 +0700

    fix

--- js/scripts.js
+++ js/scripts.js
@@ -2,45 +2,11 @@ function UI() {
     let width = $(window).width();
 
     if (width <= 768) {
-        $("#vung_choi").html(`
-        <div class="text-white text-center">
-            <h3>TomiSakae</h3>
-            <h5>Trắc Nghiệm Anime Online</h5>
-        </div>
-        <div class="vung-dang-nhap-mobile d-flex justify-content-center align-items-center">
-            <div class="text-center">
-                <h6 class="mb-4">CHƠI NGAY</h6>
-                <input type="text" class="form-control nhap-ten-mobile mb-4" id="ten_nguoi_choi"
-                    placeholder="Nhập tên người chơi..." name="ten_nguoi_choi">
-            </div>
-        </div>
-        <div class="d-flex flex-column justify-content-center align-items-center">
-            <button type="button" class="btn btn-light nut-choi-mobile rounded mt-3 mb-3">Thử Thách</button>
-            <button type="button" class="btn btn-light nut-choi-mobile rounded mb-3">Vào Phòng</button>
-            <button type="button" class="btn btn-light nut-choi-mobile rounded mb-3">Tạo Phòng</button>
-        </div>
-            `);
+        window.location.href = "mobile.html";
     }
 
     if (width > 768) {
-        $("#vung_choi").html(`
-        <div class="text-white text-center">
-            <h3>TomiSakae</h3>
-            <h5>Trắc Nghiệm Anime Online</h5>
-        </div>
-        <div class="vung-dang-nhap-pc d-flex justify-content-center align-items-center">
-            <div class="text-center">
-                <h5 class="mb-4">CHƠI NGAY</h5>
-                <input type="text" class="form-control nhap-ten-pc mb-4" id="ten_nguoi_choi"
-                    placeholder="Vui lòng nhập tên người chơi..." name="ten_nguoi_choi">
-                <div class="d-flex flex-column justify-content-center align-items-center">
-                    <button type="button" class="btn btn-outline-primary nut-choi-pc rounded mb-3">Thử Thách</button>
-                    <button type="button" class="btn btn-outline-primary nut-choi-pc rounded mb-3">Vào Phòng</button>
-                    <button type="button" class="btn btn-outline-primary nut-choi-pc rounded mb-3">Tạo Phòng</button>
-                </div>
-            </div>
-        </div>
-            `);
+        window.location.href = "pc.html";
     }
 }
 
