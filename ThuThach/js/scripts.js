4052479904b8447bf3f1ff2b0d579928091abfa3
TomiSakae <tomisakaeap1@gmail.com>
2024-06-11 07:15:48 +0700

    giao diện thử thách

---
+++ ThuThach/js/scripts.js
@@ -0,0 +1,17 @@
+let kt_che_do = 0;
+function BatDau() {
+    if (kt_che_do != 0) {
+
+    }
+    else {
+
+    }
+}
+
+$("#chon_che_do").click(function () {
+    kt_che_do = 1;
+    $("#nut_bat_dau").removeAttr('data-bs-toggle');
+    $("#nut_bat_dau").removeAttr('data-bs-target');
+    $("#chon_che_do").removeClass("che-do-choi");
+    $("#chon_che_do").addClass("che-do-choi-chon");
+});
