448bbbb663c11e0489785f06139efb92d02c4c06
TomiSakae <tomisakaeap1@gmail.com>
2024-06-18 12:33:44 +0700

    fix

--- Beta/Anime/ThuThachMoTa/js/scripts.js
+++ Beta/Anime/ThuThachMoTa/js/scripts.js
@@ -16,7 +16,7 @@ const generationConfig = {
     topP: 0.95,
     topK: 64,
     maxOutputTokens: 8192,
-    responseMimeType: "application/json",
+    responseMimeType: "text/plain",
 };
 
 async function GeminiAI(input) {
