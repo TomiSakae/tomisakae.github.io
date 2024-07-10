c85596e506f1305acba88d991562a7699ff4d64e
TomiSakae <tomisakaeap1@gmail.com>
2024-07-10 21:12:19 +0700

    Chỉnh giao diện

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import KitsuList from '../components/KitsuList';
 
 export default function Home() {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Anime Mùa Đông 2024</h1>
+    <div className="container mx-auto">
+      <h1 className="text-2xl font-bold mb-4 text-center pt-4">Anime Mùa Đông 2024</h1>
       <KitsuList />
     </div>
   );
--- src/components/KitsuList.tsx
+++ src/components/KitsuList.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import useSWRInfinite from 'swr/infinite';
 import Image from 'next/image';
@@ -16,7 +17,7 @@ const AnimeList = () => {
     return `https://kitsu.io/api/edge/anime?filter[season]=winter&filter[seasonYear]=2024&page[limit]=${PAGE_SIZE}&page[offset]=${pageIndex * PAGE_SIZE}`;
   };
 
-  const { data, error, size, setSize } = useSWRInfinite<KitsuResponse>(getKey, fetcher, {
+  const { data, error, size, setSize, isValidating } = useSWRInfinite<KitsuResponse>(getKey, fetcher, {
     revalidateOnFocus: false,
     shouldRetryOnError: false,
   });
@@ -28,7 +29,6 @@ const AnimeList = () => {
         newAnimeList = [...newAnimeList, ...page.data];
       });
 
-      // Loại bỏ các anime có key đã xuất hiện trong seenAnimeKeys và chỉ lấy subtype là 'TV'
       const filteredAnimeList = newAnimeList.filter(anime => {
         if (anime.attributes.subtype === 'TV' && !seenAnimeKeys.has(anime.id)) {
           seenAnimeKeys.add(anime.id);
@@ -39,30 +39,33 @@ const AnimeList = () => {
 
       setAnimeList(prevList => [...prevList, ...filteredAnimeList]);
 
-      // Nếu trang hiện tại có số lượng anime nhỏ hơn PAGE_SIZE, có thể đã hết dữ liệu
-      if (data[data.length - 1].data.length < PAGE_SIZE) {
-        
+      // Nếu trang hiện tại có số lượng anime nhỏ hơn PAGE_SIZE và không đang trong quá trình xác thực mới, có thể đã hết dữ liệu
+      if (data[data.length - 1].data.length < PAGE_SIZE && !isValidating) {
+        // Không cần làm gì nếu hết dữ liệu
       } else {
         setSize(size => size + 1); // Tăng kích thước để fetch trang kế tiếp
       }
     }
-  }, [data, setSize, seenAnimeKeys]);
+  }, [data, setSize, seenAnimeKeys, isValidating]);
 
   if (error) return <div>Không thể tải dữ liệu</div>;
   if (!data) return <div>Đang tải...</div>;
 
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
       {animeList.map(anime => (
-        <div key={anime.id} className="border p-4">
-          <Image 
-            src={anime.attributes.posterImage.large} 
-            alt={anime.attributes.titles.en || anime.attributes.titles.en_jp} 
-            width={550} 
-            height={780} 
-            layout="responsive"
-          />
-          <h3 className="text-lg font-semibold mt-2">{anime.attributes.titles.en || anime.attributes.titles.en_jp}</h3>
+        <div key={anime.id} className="px-4 py-3 hover:text-green-500 text-gray-500">
+          <Link href="/anime">
+            <Image 
+              src={anime.attributes.posterImage.large} 
+              alt={anime.attributes.titles.en || anime.attributes.titles.en_jp} 
+              width={550} 
+              height={780} 
+              layout="responsive"
+              className="rounded-xl"
+            />
+            <h3 className="text-xs font-semibold mt-2 overflow-hidden truncate">{anime.attributes.titles.en || anime.attributes.titles.en_jp}</h3>
+          </Link>
         </div>
       ))}
     </div>
