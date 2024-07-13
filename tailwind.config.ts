ed8a9cef7c31f84d96aa7e5b798bbb43daec1279
TomiSakae <tomisakaeap1@gmail.com>
2024-07-13 13:45:14 +0700

    fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: 'class',
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,7 +10,7 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        
+
       },
     },
   },
