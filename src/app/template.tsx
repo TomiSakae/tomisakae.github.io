3327561feec7ecca70e1fb28ab627d89c206d0c8
TomiSakae <tomisakaeap1@gmail.com>
2024-07-14 19:37:33 +0700

    update

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -9,7 +9,7 @@ export default function Template({
 }) {
     return (
         <motion.div
-            initial={{ y: 20, opacity: 0 }}
+            initial={{ y: -20, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ ease: "easeInOut", duration: 0.75 }}
         >
