0bd1cea52298ffa8aaea1383248fa3dabc6b08c7
TomiSakae <tomisakaeap1@gmail.com>
2024-07-14 12:54:24 +0700

    update

--- src/hooks/useViewport.ts
+++ src/hooks/useViewport.ts
@@ -1,14 +1,25 @@
-// hooks/useViewport.ts
 import { useState, useEffect } from 'react';
 
 export function useViewport() {
-    const [width, setWidth] = useState<number>(window.innerWidth);
+    const [width, setWidth] = useState<number>(0); // Initial width can be set to 0 or any default value
 
     useEffect(() => {
-        const handleResize = () => setWidth(window.innerWidth);
+        // Check if window is defined to avoid SSR errors
+        if (typeof window !== 'undefined') {
+            setWidth(window.innerWidth);
+        }
+
+        const handleResize = () => {
+            if (typeof window !== 'undefined') {
+                setWidth(window.innerWidth);
+            }
+        };
+
         window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []); // Empty dependency array ensures useEffect runs only once
 
     return { width };
 }
