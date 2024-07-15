d8cb39bc6e3d379273957c4e41b1ee9aa8778388
TomiSakae <tomisakaeap1@gmail.com>
2024-07-15 11:10:05 +0700

    update

--- src/app/tomisakae/page.tsx
+++ src/app/tomisakae/page.tsx
@@ -1,11 +1,15 @@
+'use client'
 import Image from 'next/image';
 import { FaFacebook } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { SiZalo } from "react-icons/si";
 import Link from 'next/link';
 import StarsCanvas from "../../components/ThreeBackground"
+import { motion } from "framer-motion";
 
 const TomiSakae = () => {
+    const text = "TomiSakae".split("");
+    const next = "NextJS Developer".split("");
     return (
         <div className="flex flex-col text-center font-bold pb-12 items-center mx-6 justify-center text-white h-screen">
             <StarsCanvas />
@@ -14,13 +18,42 @@ const TomiSakae = () => {
                 alt="TomiSakae"
                 width={200}
                 height={200}
-                className="rounded-full"
+                className="rounded-full box-tomi"
             />
-            <h1 className="text-2xl mt-4">TomiSakae</h1>
+            <h1 className="text-2xl mt-4">
+                {text.map((el, i) => (
+                    <motion.span
+                        initial={{ opacity: 0.5 }}
+                        animate={{ opacity: 1 }}
+                        transition={{
+                            duration: 0.5,
+                            delay: i / 10,
+                            repeat: Infinity,
+                            repeatType: "mirror"
+
+                        }}
+                        key={i}
+                    >
+                        {el}{""}
+                    </motion.span>
+                ))}
+            </h1>
 
             <p className="text-xl font-bold relative">
                 <span className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent">
-                    NextJS Developer
+                    {next.map((el, i) => (
+                        <motion.span
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{
+                                duration: 0.75,
+                                delay: i / 10,
+                            }}
+                            key={i}
+                        >
+                            {el}{""}
+                        </motion.span>
+                    ))}
                 </span>
             </p>
 
