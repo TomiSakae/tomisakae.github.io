87d74f617b3eb7774d2e9881680fe5038167f435
TomiSakae <tomisakaeap1@gmail.com>
2024-07-15 10:23:21 +0700

    update

--- src/app/tomisakae/page.tsx
+++ src/app/tomisakae/page.tsx
@@ -1,8 +1,14 @@
 import Image from 'next/image';
+import { FaFacebook } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { SiZalo } from "react-icons/si";
+import Link from 'next/link';
+import StarsCanvas from "../../components/ThreeBackground"
 
 const TomiSakae = () => {
     return (
-        <div className="flex flex-col text-center pb-12 items-center mx-6 justify-center text-white h-screen">
+        <div className="flex flex-col text-center font-bold pb-12 items-center mx-6 justify-center text-white h-screen">
+            <StarsCanvas />
             <Image
                 src="/tomisakae.jpg"
                 alt="TomiSakae"
@@ -11,7 +17,13 @@ const TomiSakae = () => {
                 className="rounded-full"
             />
             <h1 className="text-2xl mt-4">TomiSakae</h1>
-            <p className="text-lg">Số lượng truy cập trang Web!</p>
+
+            <p className="text-xl font-bold relative">
+                <span className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent">
+                    NextJS Developer
+                </span>
+            </p>
+
             <Image
                 src="https://count.getloli.com/get/@:tomisakaewebbeta?theme=rule34"
                 alt="Moe"
@@ -19,6 +31,17 @@ const TomiSakae = () => {
                 height={350}
                 className="mt-4"
             />
+            <div className="flex items-center space-x-6 text-lg mt-6 mb-4">
+                <Link href="https://www.facebook.com/TomiSakaeAnime/" target="_blank" className="text-white">
+                    <FaFacebook className="w-8 h-8" />
+                </Link>
+                <Link href="https://github.com/TomiSakae" className="text-white">
+                    <FaGithub className="w-8 h-8" />
+                </Link>
+                <Link href="https://zalo.me/0762605309" target="_blank" className="bg-white text-black rounded-full flex items-center justify-center w-8 h-8">
+                    <SiZalo className="w-6 h-6" />
+                </Link>
+            </div>
         </div>
     );
 }
