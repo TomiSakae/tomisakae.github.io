b0767a42531e6a820e38c06d8a3f74a416c32a4c
TomiSakae <tomisakaeap1@gmail.com>
2024-07-16 10:15:55 +0700

    test

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -32,7 +32,7 @@ const Live2DModelComponent = () => {
             const model = await Live2DModel.from('https://cdn.jsdelivr.net/gh/Eikanya/Live2d-model/%E7%A2%A7%E8%93%9D%E8%88%AA%E7%BA%BF%20Azue%20Lane/Azue%20Lane(JP)/abeikelongbi_3/abeikelongbi_3.model3.json');
 
             app.stage.addChild(model as unknown as PIXI.DisplayObject); // Chuyển kiểu về DisplayObject
-            (model as any).y = innerHeight * 0.10;
+            (model as any).y = innerHeight * 0.09;
             (model as any).position.x = -125;
             (model as any).scale.set(0.1); // Cast về any để tránh lỗi TypeScript
             // Cấu hình mô hình
@@ -60,15 +60,9 @@ const Live2DModelComponent = () => {
             <style jsx global>{`
                 #canvas {
                     background-image: url('/background1.avif'); */
-                   background-size: cover; /* Đảm bảo hình ảnh nền được phủ đầy canvas */
+                    background-size: cover; /* Đảm bảo hình ảnh nền được phủ đầy canvas */
                     background-position: center; /* Canh giữa hình ảnh */
                     background-repeat: no-repeat; /* Không lặp lại hình ảnh */
-                    /* Thiết lập các thuộc tính khác cho canvas */
-                    width: 100vw; /* Chiều rộng bằng 100% viewport width */
-                    height: 100vh; /* Chiều cao bằng 100% viewport height */
-                    position: fixed; /* Đảm bảo canvas cố định trong viewport */
-                    top: 0;
-                    left: 0;
                     z-index: -1; /* Đặt z-index để đặt nó phía sau các phần tử khác trên trang */
                 }
                 .gradient-background {
