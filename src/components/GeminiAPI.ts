4f93d87d74330b588242f7ad20c8cd823b484704
TomiSakae <tomisakaeap1@gmail.com>
2024-07-16 21:52:20 +0700

    test

--- src/components/GeminiAPI.ts
+++ src/components/GeminiAPI.ts
@@ -1,3 +1,4 @@
+// utils/generateChatResponse.ts
 import {
     GoogleGenerativeAI,
     HarmCategory,
@@ -5,7 +6,7 @@ import {
 } from "@google/generative-ai";
 
 const MODEL_NAME = "gemini-1.5-pro";
-const API_KEY = process.env.NEXT_PUBLIC_GEMINI_API_KEY as string || "AIzaSyAurxDlGztVcK-gWWpduwbucckHaxt0iIk"; // Update with your API key
+const API_KEY = process.env.NEXT_PUBLIC_GEMINI_API_KEY as string || "AIzaSyAurxDlGztVcK-gWWpduwbucckHaxt0iIk";
 
 export async function generateChatResponse(prompt: string): Promise<string> {
     const genAI = new GoogleGenerativeAI(API_KEY);
@@ -18,36 +19,33 @@ export async function generateChatResponse(prompt: string): Promise<string> {
         maxOutputTokens: 2048,
     };
 
-    // Assume the chat history is stored in localStorage
-    let chatHistory = JSON.parse(window.localStorage.getItem('chatHistory') || '[]');
-    // Kiểm tra nếu chatHistory vượt quá 100 phần tử
-    if (chatHistory.length > 100) {
-        // Bỏ 2 phần tử đầu tiên của mảng
-        chatHistory = chatHistory.slice(2);
-    }
     const chat = model.startChat({
         generationConfig,
-        history: chatHistory, // Load chat history from localStorage
+        history: [
+            {
+                role: "user",
+                parts: [{ text: "Gemini sẽ luôn nói tiếng việt trừ khi nào có các yêu cầu về ngôn ngữ khác" }],
+            },
+            {
+                role: "model",
+                parts: [{ text: "Tôi hiểu rồi. Từ giờ trở đi, tôi sẽ luôn giao tiếp bằng tiếng Việt, trừ khi bạn yêu cầu tôi sử dụng ngôn ngữ khác." }],
+            },
+            {
+                "role": "user",
+                "parts": [
+                    { text: "Gemini sẽ luôn chỉ trả lời trên 1 dòng" }
+                ],
+            },
+            {
+                "role": "model",
+                "parts": [
+                    { text: "Tôi hiểu rồi, từ giờ tôi sẽ luôn cố gắng trả lời bạn chỉ trong một dòng." }
+                ],
+            },
+        ],
     });
 
     const result = await chat.sendMessage(prompt);
     const response = result.response;
-
-    // Update chat history
-    chatHistory.push({
-        role: "user",
-        parts: [{ text: prompt }],
-    });
-    chatHistory.push({
-        role: "model",
-        parts: [{ text: response.text() }],
-    });
-    window.localStorage.setItem('chatHistory', JSON.stringify(chatHistory));
-
-    // Đoạn code để xóa hai mảng cuối trong chatHistory
-    chatHistory.splice(-2);
-
-    // Lưu lại chatHistory đã được cập nhật sau khi xóa
-    window.localStorage.setItem('chatHistory', JSON.stringify(chatHistory));
     return response.text();
 }
