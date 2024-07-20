041cd365a1a92220cfad4cc2f021959eb29a0851
TomiSakae <tomisakaeap1@gmail.com>
2024-07-20 13:23:39 +0700

    Thêm model

--- src/components/Live2D.ts
+++ src/components/Live2D.ts
@@ -8,23 +8,23 @@ const modelData = {
             modelid: '1'
         },
         {
+            model: '/live2d/models/abeikelongbi_3/abeikelongbi_3.model3.json',
+            modelname: 'HMS Abercrombie (F109)',
+            modelid: '2'
+        },
+        {
             model: '/live2d/models/adaerbote_2/adaerbote_2.model3.json',
             modelname: 'KMS Prinz Adalbert',
-            modelid: '2'
+            modelid: '3'
         },
         {
             model: '/live2d/models/aerbien_3/aerbien_3.model3.json',
             modelname: 'Albion',
-            modelid: '3'
+            modelid: '4'
         },
         {
             model: '/live2d/models/aersasi_2/aersasi_2.model3.json',
             modelname: 'Essex',
-            modelid: '4'
-        },
-        {
-            model: '/live2d/models/abeikelongbi_3_hx/abeikelongbi_3_hx.model3.json',
-            modelname: 'HMS Abercrombie (F109)',
             modelid: '5'
         }
     ]
@@ -50,16 +50,21 @@ export function Live2d(id: number): Live2dResult {
             setScale = 0.1;
             break;
         case 2:
+            setY = 28;
+            setX = -125;
+            setScale = 0.1;
+            break;
+        case 3:
             setY = -89;
             setX = -200;
             setScale = 0.1;
             break;
-        case 3:
+        case 4:
             setY = -113;
             setX = -233;
             setScale = 0.1;
             break;
-        case 4:
+        case 5:
             setY = -145;
             setX = -264;
             setScale = 0.1;
