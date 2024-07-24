ff84753f0f17723f4a20370764d5d8df54658ffa
TomiSakae <tomisakaeap1@gmail.com>
2024-07-24 21:26:32 +0700

    Thêm 1 model mới

--- public/live2d/steam_models/img.ts
+++ public/live2d/steam_models/img.ts
@@ -4,6 +4,12 @@ const images = [
         "id": 1,
         "src": "/live2d/steam_models/1/background.png",
         "alt": "Image 1"
+    },
+    
+    {
+        "id": 2,
+        "src": "/live2d/steam_models/2/background.png",
+        "alt": "Image 2"
     }
 
 ];
