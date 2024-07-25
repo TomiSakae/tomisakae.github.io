ab2f361b660a73bda9133f4511c63760b08719b8
TomiSakae <tomisakaeap1@gmail.com>
2024-07-25 17:26:08 +0700

    update

--- src/app/tomisakae/page.tsx
+++ src/app/tomisakae/page.tsx
@@ -1,9 +1,5 @@
 'use client'
 import Image from 'next/image';
-import { FaFacebook } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { SiZalo } from "react-icons/si";
-import Link from 'next/link';
 import StarsCanvas from "../../components/ThreeBackground"
 import { motion } from "framer-motion";
 
@@ -58,23 +54,12 @@ const TomiSakae = () => {
             </p>
 
             <Image
-                src="https://count.getloli.com/get/@:tomisakaewebbeta?theme=rule34"
+                src="https://count.getloli.com/get/@:TomiSakaeWeb?theme=rule34"
                 alt="Moe"
                 width={350}
                 height={350}
                 className="mt-4"
             />
-            <div className="flex items-center space-x-6 text-lg mt-6 mb-4">
-                <Link href="https://www.facebook.com/TomiSakaeAnime/" target="_blank" className="text-white">
-                    <FaFacebook className="w-8 h-8" />
-                </Link>
-                <Link href="https://github.com/TomiSakae" target="_blank" className="text-white">
-                    <FaGithub className="w-8 h-8" />
-                </Link>
-                <Link href="https://zalo.me/0762605309" target="_blank" className="bg-white text-black rounded-full flex items-center justify-center w-8 h-8">
-                    <SiZalo className="w-6 h-6" />
-                </Link>
-            </div>
         </div>
     );
 }
