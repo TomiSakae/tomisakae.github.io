989a138792757f4170c162731114c9ae7de5b262
TomiSakae <tomisakaeanime@gmail.com>
2024-09-15 21:22:18 +0700

    Thêm icon ứng dụng cài đặt

--- src/app/AniPhone/page.tsx
+++ src/app/AniPhone/page.tsx
@@ -1,10 +1,16 @@
 'use client'
 import { useState, useEffect } from 'react';
 import { LiaBatteryFullSolid } from "react-icons/lia";
+import { FaRegSquare } from "react-icons/fa6";
+import { CiMenuBurger } from "react-icons/ci";
+import { RiPlayReverseLargeLine } from "react-icons/ri";
+import Image from 'next/image';
 
 const AniPhone = () => {
     const [backgroundImage, setBackgroundImage] = useState('');
     const [isLoading, setIsLoading] = useState(true);
+    // Thêm state mới
+    const [showHomeScreen, setShowHomeScreen] = useState(false);
 
     useEffect(() => {
         const fetchBackgroundImage = async () => {
@@ -19,7 +25,7 @@ const AniPhone = () => {
                     const image = data.images[0];
                     setBackgroundImage(image.url);
                     // Tạo một Image object để đảm bảo ảnh đã tải xong
-                    const img = new Image();
+                    const img = document.createElement('img');
                     img.onload = () => setIsLoading(false);
                     img.src = image.url;
                 } else {
@@ -34,6 +40,13 @@ const AniPhone = () => {
         fetchBackgroundImage();
     }, []);
 
+    // Thêm hàm xử lý click
+    const handleScreenClick = () => {
+        if (!isLoading) {
+            setShowHomeScreen(true);
+        }
+    };
+
     if (isLoading) {
         return (
             <div className="bg-black h-screen flex items-center justify-center">
@@ -46,23 +59,66 @@ const AniPhone = () => {
         <div
             className="h-screen w-full bg-cover bg-center"
             style={{ backgroundImage: `url(${backgroundImage})` }}
+            onClick={handleScreenClick} // Thêm sự kiện click
         >
-            <div className='flex justify-between items-center text-white mx-4'>
-                <div className='flex items-center'>
-                </div>
-                <div className='flex items-center'>
-                    <LiaBatteryFullSolid className='mr-1 mt-[2px] text-2xl' />
-                    <span className=''>100%</span>
-                </div>
-            </div>
-            <div className='mt-12 flex flex-col items-center text-white mx-4'>
-                <div className='text-4xl font-bold'>
-                    {new Date().toLocaleTimeString('vi-VN', { hour: '2-digit', minute: '2-digit' })}
-                </div>
-                <div className='text-xl'>
-                    {new Date().toLocaleDateString('vi-VN', { weekday: 'long', day: '2-digit', month: '2-digit', year: 'numeric' })}
-                </div>
-            </div>
+            {!showHomeScreen ? (
+                <>
+                    <div className='flex justify-between items-center text-white mx-4 pt-1'>
+                        <div className='flex items-center'>
+
+                        </div>
+                        <div className='flex items-center'>
+                            <LiaBatteryFullSolid className='mr-1 mt-[2px] text-2xl' />
+                            <span className=''>100%</span>
+                        </div>
+                    </div>
+                    <div className='mt-14 flex flex-col items-center text-white mx-4'>
+                        <div className='text-4xl font-bold'>
+                            {new Date().toLocaleTimeString('vi-VN', { hour: '2-digit', minute: '2-digit' })}
+                        </div>
+                        <div className='text-xl'>
+                            {new Date().toLocaleDateString('vi-VN', { weekday: 'long', day: '2-digit', month: '2-digit', year: 'numeric' })}
+                        </div>
+                    </div>
+                </>
+            ) : (
+                <>
+                    <div className='flex justify-between items-center text-black mx-4 pt-1'>
+                        <div className='flex items-center'>
+                            <div className='text-md'>
+                                {new Date().toLocaleTimeString('vi-VN', { hour: '2-digit', minute: '2-digit' })}
+                            </div>
+                        </div>
+                        <div className='flex items-center'>
+                            <LiaBatteryFullSolid className='mr-1 mt-[2px] text-2xl' />
+                            <span className=''>100%</span>
+                        </div>
+                    </div>
+                    <div className='absolute bottom-0 left-0 right-0 flex justify-between items-center text-white pb-1 mx-6'>
+                        <div className='flex-1 flex justify-center p-2 rounded-full hover:bg-gray-200 hover:bg-opacity-20 transition-all duration-300'>
+                            <div className='flex flex-col items-center'>
+                                <CiMenuBurger className='text-md' />
+                            </div>
+                        </div>
+                        <div className='flex-1 flex justify-center p-2 rounded-full hover:bg-gray-200 hover:bg-opacity-20 transition-all duration-300'>
+                            <div className='flex flex-col items-center'>
+                                <FaRegSquare className='text-md' />
+                            </div>
+                        </div>
+                        <div className='flex-1 flex justify-center p-2 rounded-full hover:bg-gray-200 hover:bg-opacity-20 transition-all duration-300'>
+                            <div className='flex flex-col items-center'>
+                                <RiPlayReverseLargeLine className='text-lg' />
+                            </div>
+                        </div>
+                    </div>
+                    <div className='absolute bottom-12 left-4 right-4 flex flex-row items-end text-white text-md font-[500] justify-between'>
+                        <div className='flex flex-col items-center cursor-pointer'>
+                            <Image src="/setting.png" alt="Settings" width={64} height={64} />
+                            <span>Cài đặt</span>
+                        </div>
+                    </div>
+                </>
+            )}
         </div>
     );
 };
