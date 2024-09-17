8fd5046ce8c685921500ef462c2df19026125831
TomiSakae <tomisakaeanime@gmail.com>
2024-09-17 22:45:28 +0700

    fix

--- src/app/AniPhone/browser/page.tsx
+++ src/app/AniPhone/browser/page.tsx
@@ -22,7 +22,7 @@ const BrowserPage = () => {
             let finalUrl = newUrl;
             setDisplayUrl(newUrl);
             if (newUrl === 'aniw://doctruyenkiemtien.ani') {
-                finalUrl = 'http://tomisakae.github.io/AniPhone/browser/custom/doctruyen';
+                finalUrl = 'https://tomisakae.github.io/AniPhone/browser/custom/doctruyen';
             }
             setHistory([...history.slice(0, currentIndex + 1), finalUrl]);
             setCurrentIndex(currentIndex + 1);
